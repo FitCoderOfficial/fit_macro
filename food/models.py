c1df5c5e156dfefa3fad4de4606b22ba5af6e513
Shin-Sae-byeok <toqur1219@naver.com>
2023-02-11 00:34:53 +0900

    feat:create food model

---
+++ food/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+
+UNIT_CHOICES = [
+        ('g', 'g'),
+        ('ml', 'ml'),
+    ]
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Food(models.Model):
+       
+
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    year_of_manufacture = models.PositiveIntegerField(blank=True, null=True)
+    manufacturer = models.CharField(max_length=100, blank=True, null=True)
+    main_category = models.CharField(max_length=100)
+    detailed_classification = models.CharField(max_length=100)
+    serving_size = models.PositiveIntegerField()
+    serving_unit = models.CharField(choices=UNIT_CHOICES, max_length=2)
+    total_contents = models.PositiveIntegerField(blank=True, null=True)
+    total_calories = models.FloatField(blank=True, null=True)
+    protein = models.FloatField(blank=True, null=True)
+    fat = models.FloatField(blank=True, null=True)
+    carbohydrates = models.FloatField(blank=True, null=True)
+    total_sugars = models.FloatField(blank=True, null=True)
+    sodium = models.FloatField(blank=True, null=True)
+    cholesterol = models.FloatField(blank=True, null=True)
+    saturated_fatty_acids = models.FloatField(blank=True, null=True)
+    trans_fatty_acids = models.FloatField(blank=True, null=True)
+    
+    def __str__(self):
+        return self.name
